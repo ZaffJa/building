dca2e1031b002d9b4465551d8f6c491597c7eef3
Zafri Zulkipli <zafrizulkipli@gmail.com>
2017-01-05 10:38:38 +0800

    Added favicon.

--- resources/views/layouts/tenant.blade.php
+++ resources/views/layouts/tenant.blade.php
@@ -6,6 +6,8 @@
     <title>Tenants Dashboard</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
     <!-- Bootstrap 3.3.6 -->
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
     <!-- Font Awesome -->
